03dc42bb7d865ad391d8919840cce18d94facd5f
Pol <79444349+fakefarreraspol@users.noreply.github.com>
2024-10-10 00:20:49 +0200

    sprites and more

--- Scripts/Game/Hand.gd
+++ Scripts/Game/Hand.gd
@@ -2,13 +2,17 @@ extends Node
 
 var handCards = []
 
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass
 
 func AddCard(card: Card) -> void:
 	handCards.append(card)
+	
 	#print("Car added!" + str(len(handCards)))
+
 
 func RemoveCard(card: Card) -> void:
 	handCards.erase(card)
--- Scripts/UI/UICard.gd
+++ Scripts/UI/UICard.gd
@@ -5,12 +5,6 @@ var num_columns = 13
 var num_rows = 5
 var data: Card
 
-#func _init(card: Card) -> void:
-	#data = card
-	#update_sprite(0)
-
-#func _ready() -> void:
-	#update_sprite(4)
 
 func update_sprite(index: int) -> void:
 	var cardSize = GetCardSize()	
@@ -36,3 +30,42 @@ func GetCardSize() -> Vector2:
 	
 	print(Vector2(cardWidth, cardHeight))
 	return Vector2(cardWidth, cardHeight)
+
+
+
+# 10 (0-9) +2 Reverse Skip
+func InitCard(card: Card) -> void:
+	var calculatedIndex : int
+	if card.card_type == card.cardType.NUMBER:
+		if card.card_color == card.cardColor.RED:
+			calculatedIndex = card.cardNumber
+		elif card.card_color == card.cardColor.GREEN:
+			calculatedIndex = card.cardNumber + 13
+		elif card.card_color == card.cardColor.BLUE:
+			calculatedIndex = card.cardNumber + 26 
+		elif card.card_color == card.cardColor.YELLOW:
+			calculatedIndex = card.cardNumber + 39 
+	elif card.card_type == card.cardType.SKIP:
+		if card.card_color == card.cardColor.RED:
+			calculatedIndex = 13
+		elif card.card_color == card.cardColor.GREEN:
+			calculatedIndex = 26
+		elif card.card_color == card.cardColor.BLUE:
+			calculatedIndex = 39
+		elif card.card_color == card.cardColor.YELLOW:
+			calculatedIndex = 52
+	elif card.card_type == card.cardType.DRAW_TWO:
+		if card.card_color == card.cardColor.RED:
+			calculatedIndex = 11
+		elif card.card_color == card.cardColor.GREEN:
+			calculatedIndex = 24
+		elif card.card_color == card.cardColor.BLUE:
+			calculatedIndex = 37
+		elif card.card_color == card.cardColor.YELLOW:
+			calculatedIndex = 49
+	elif card.card_type == card.cardType.WILD:
+		calculatedIndex = 53
+	elif card.card_type == card.cardType.WILD_DRAW_FOUR:
+		calculatedIndex = 58
+		
+	update_sprite(calculatedIndex)
