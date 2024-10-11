7d7ed387c9330073d6f4fe2ad0eb761242d73832
Pol <79444349+fakefarreraspol@users.noreply.github.com>
2024-10-11 13:58:13 +0200

    bg

--- Scripts/UI/card_container.gd
+++ Scripts/UI/card_container.gd
@@ -24,3 +24,8 @@ func _on_mouse_exited() -> void:
 		var tween2 = get_tree().create_tween()
 		tween.tween_property(self, "position", startPosition, 0.2)
 		tween2.tween_property(self, "scale", Vector2(1,1), 0.2)
+
+
+func _on_child_exiting_tree(node: Node) -> void:
+	print("XD")
+	pass
